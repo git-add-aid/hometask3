0d208c6090633738b85e70dfd2252e7fad47a1c7
Taras <taras.trachuk93@gmail.com>
2020-11-16 23:45:54 +0200

    1part without cheked

---
+++ script.js
@@ -0,0 +1,30 @@
+// var question = confirm('У тебя есть браться или сетсры?');
+// if (question == true){
+//   var relativesNames = prompt('Напиши их имена через , ')
+//   alert('Привет  ' + relativesNames + ' !')
+// }else{
+//   var cousinQuestion = confirm('а 2х юродные?')
+//   if(cousinQuestion == true){
+//     var cousinNames = prompt('Напиши их имена через ,')
+//     alert('Привет ' + cousinNames + ' !')
+//   }else {
+//     alert('Мне очень жаль :( ')
+//   }
+// }
+
+
+
+
+var question = confirm('У тебя есть браться или сетсры?');
+if (question == true){
+  var relativesNames = prompt('Напиши их имена через , ')
+  alert('Привет  ' + relativesNames + ' !')
+}else{
+  var cousinQuestion = confirm('а 2х юродные?')
+  if(cousinQuestion == true){
+    var cousinNames = prompt('Напиши их имена через ,')
+    alert('Привет ' + cousinNames + ' !')
+  }else {
+    alert('Мне очень жаль :( ')
+  }
+}
