b51c2589160ed3a44adf6968eefe4fce7f44e373
Taras <taras.trachuk93@gmail.com>
2020-11-18 22:41:15 +0200

    LET,Voszrast

--- script.js
+++ script.js
@@ -1,30 +1,16 @@
-// var question = confirm('У тебя есть браться или сетсры?');
-// if (question == true){
-//   var relativesNames = prompt('Напиши их имена через , ')
-//   alert('Привет  ' + relativesNames + ' !')
-// }else{
-//   var cousinQuestion = confirm('а 2х юродные?')
-//   if(cousinQuestion == true){
-//     var cousinNames = prompt('Напиши их имена через ,')
-//     alert('Привет ' + cousinNames + ' !')
-//   }else {
-//     alert('Мне очень жаль :( ')
-//   }
-// }
-
-
-
-
-var question = confirm('У тебя есть браться или сетсры?');
-if (question == true){
-  var relativesNames = prompt('Напиши их имена через , ')
-  alert('Привет  ' + relativesNames + ' !')
-}else{
-  var cousinQuestion = confirm('а 2х юродные?')
-  if(cousinQuestion == true){
-    var cousinNames = prompt('Напиши их имена через ,')
-    alert('Привет ' + cousinNames + ' !')
-  }else {
-    alert('Мне очень жаль :( ')
-  }
+var inputAge = +prompt("Напиши мне свой возраст!",'');
+console.log(inputAge);
+if (isNaN(inputAge)) {
+  alert('Нужно ввести свой возраст числом!')
+} else if (inputAge === 0) {
+  alert('Та введи ты уже число!!!')
+} else {
+  alert('хорошо! , Я вычисляю твой возраст ')
+}
+if (inputAge % 10 >= 5 || inputAge % 10 === 0 || (inputAge % 100 >= 11 && inputAge % 100 <= 14)) {
+  alert('Тебе ' + inputAge +' лет!')
+} else if (inputAge % 10 === 1) {
+  alert('Тебе ' + inputAge + ' год!')
+} else {
+  alert('Тебе ' + inputAge + ' года!')
 }
